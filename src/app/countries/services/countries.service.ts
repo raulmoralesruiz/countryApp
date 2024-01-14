bb18c2defe408643eac07eb4d0ae06f6e04a725c
Raúl Morales <raulmdaw@gmail.com>
2024-01-14 23:42:31 +0100

    135. Refactorizaciones en nuestro servicio

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,7 +10,11 @@ export class CountriesService {
   constructor(private http: HttpClient) {}
 
   private getCountriesRequest(url: string): Observable<Country[]> {
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(() => of([])),
+        delay(2000)
+      );
   }
 
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
