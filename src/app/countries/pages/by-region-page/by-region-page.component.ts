99d0fb2ad117ede4bfd8bbdc53c9858d0649e485
Raúl Morales <raulmdaw@gmail.com>
2024-01-15 20:37:00 +0100

    140. Buscar por region - Optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(searchedText: string): void {
-    this.countriesService.searchRegion(searchedText).subscribe((countries) => {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+
+    this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
     });
   }
